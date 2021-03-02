d7ba9716f7e7df89e4b0112126dea6b3c7acf221
jayh <jayh@palittoconsulting.com>
2021-03-02 17:14:26 -0500

    Update citylinksignupvue.js

--- citylinksignupvue.js
+++ citylinksignupvue.js
@@ -169,7 +169,7 @@ var app = new Vue({
 		axios
 			.get('https://jayh13.github.io/CityLink/citylinkserviceplans.json')
 			.then(response => {
-				this.services = JSON.parse(response.data.replace(/(\r\n|\n|\r)/gm, ""));
+				this.services = response.data;
 			});
 	},
 	computed: {
